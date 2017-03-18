c4ef23312bf91abf821ec1085b37118e3b1174a6
Fred Soglohu <fsoglohu@gmail.com>
2017-03-18 17:20:35 +0000

    Initializing the main class

---
+++ guess.py
@@ -0,0 +1,17 @@
+
+import random
+
+class myself(object):
+	"""docstring for ClassName"""
+	def __init__(self):
+		super(ClassName, self).__init__()
+		self.name = "Freddy"
+		self.full_name = "Fred Soglohu"
+		self.age = "25"
+		self.gender = "male"
+		self.nationality = "Ghanaian"
+		self.relationship_status = "single"
+
+		
+		
+
