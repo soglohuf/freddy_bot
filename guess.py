acf41f2a101a6599c1ae2a540b406c1fb78bb6a0
Fred Soglohu <fsoglohu@gmail.com>
2017-03-19 14:05:30 +0000

    changed to return a random number from 1 to 10

--- guess.py
+++ guess.py
@@ -1,16 +1,12 @@
 
 import random
 
-class myself(object):
-	"""docstring for ClassName"""
-	def __init__(self):
-		super(ClassName, self).__init__()
-		self.name = "Freddy"
-		self.full_name = "Fred Soglohu"
-		self.age = "25"
-		self.gender = "male"
-		self.nationality = "Ghanaian"
-		self.relationship_status = "single"
+def number():
+	return random.randint(1,10)
+
+
+
+print(number())
 
 		
 		
